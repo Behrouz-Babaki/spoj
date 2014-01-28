3b47e7cc0c74a287589bef6a0a2f46d53e73c67d
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-01-29 00:04:18 +0100

    further working on 244

--- 244/244.cpp
+++ 244/244.cpp
@@ -11,7 +11,16 @@ using std::cerr;
 using std::vector;
 using std::priority_queue;
 
-void find_mis(const vector<vector<unsigned int> >&, const vector<unsigned int>&);
+struct Node{
+  unsigned int vIndex;
+  unsigned int weight;
+};
+
+vector<unsigned int> weights;
+vector<vector<unsigned int> > edges;
+double global_best;
+
+void search (const vector<Node>&, const vector<Node>&, double, double);
 
 int main(void){
   int num_cases;
@@ -29,11 +38,19 @@ int main(void){
       exit(EXIT_FAILURE);
     }
     
-    vector<unsigned int> weights(num_vertices, -1);
-    for (int vCounter = 0; vCounter < num_vertices; vCounter++)
+    vector<Node> nodes(num_vertices);
+    weights.resize(num_vertices, -1);
+    double sum = 0;
+    for (int vCounter = 0; vCounter < num_vertices; vCounter++){
       cin >> weights[vCounter];
+      Node current_node;
+      current_node.vIndex = vCounter;
+      current_node.weight = weights[vCounter];
+      sum += current_node.weight;
+      nodes.push_back(current_node);
+    }
 
-    vector<vector<unsigned int> > edges(num_vertices);
+    edges.resize(num_vertices);
     for (int edgeCounter = 0; edgeCounter < num_edges; edgeCounter++){
       unsigned int firstVertex, secondVertex;
       cin >> firstVertex >> secondVertex >> ws;
@@ -42,12 +59,41 @@ int main(void){
       edges[firstVertex].push_back(secondVertex);
       edges[secondVertex].push_back(firstVertex);
     }
-    find_mis(edges, weights);
+    
+    global_best = 0;
+    vector<Node> init;
+    search (init, nodes, 0, sum);
+    cout << global_best << endl;
   }
 
   return 0;
 }
 
-void find_mis(const vector<vector<unsigned int> >& edges, const vector<unsigned int>& w){
-  priority_queue<unsigned int> weights (w.begin(), w.end());
+void search (const vector<Node>& in_set, const vector<Node>& candidates, double inWeightSum, double candWeightSum){
+
+  if (candidates.empty()){
+    if (inWeightSum > global_best)
+      global_best = inWeightSum;
+    return;
+  }
+  
+  vector<Node> cand (candidates);
+
+  if (inWeightSum + candWeightSum > global_best){
+    vector<Node> in (in_set);
+    while (!cand.empty()){
+      Node nextNode = cand.back();
+      cand.pop_back();
+      in.push_back(nextNode);
+      candWeightSum -= nextNode.weight;
+      double partial = inWeightSum + nextNode.weight;
+      //This is crazy! Why are we copying this everywhere?
+      vector<Node> cand2 (cand);
+      //TODO remove those nodes from cand2 which are adjacent to nextNode
+      search(in, cand2, partial, candWeightSum);
+      in.pop_back();
+    }
+  }
+    return;
 }
+
