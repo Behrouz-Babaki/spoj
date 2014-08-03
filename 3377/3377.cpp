d15c4ddf2af3e3e27a4d8ba6e989f2d26acff6f0
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-08-03 17:40:40 +0200

    3377 wrong answer

--- 3377/3377.cpp
+++ 3377/3377.cpp
@@ -1,3 +1,4 @@
+// problem 3377. A Bug's Life -- problem code: BUGLIFE
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -12,6 +13,9 @@ using std::pair;
 using std::make_pair;
 
 void printInteractions (const vector<vector<unsigned int> >&, const vector<pair<int, int> >&);
+bool suspicious (const vector<vector<unsigned int> >&, const vector<int>&, int, vector<char>);
+
+bool pairComp (const pair<int,int>& left, const pair<int, int>& right){ return left.second > right.second;}
 
 int main(void) {
   int numCases;
@@ -24,29 +28,71 @@ int main(void) {
       {
 	int first, second;
 	cin >> first >> second;
-	interactions[first-1].push_back(second);
-	interactions[second-1].push_back(first);
+	interactions[first-1].push_back(second-1);
+	interactions[second-1].push_back(first-1);
       }
 
     vector<pair<int, int> > vecSizes (numBugs);
     for (int counter = 0; counter < numBugs; counter++)
       vecSizes[counter] = make_pair (counter, interactions[counter].size());
     
-    sort (vecSizes.begin(), vecSizes.end(), [] (const pair<int,int>& left, const pair<int, int>& right){ return left.second > right.second;});
-    printInteractions (interactions, vecSizes);
-    cout << endl;
-    
+    sort (vecSizes.begin(), vecSizes.end(), pairComp);
+    vector<int> order (numBugs);
+    for (int counter = 0; counter < numBugs; counter++)
+      order[counter] = vecSizes[counter].first;
+
+    vector<char> sex(numBugs, 2);
+    cout << "Scenario #" << caseCounter+1 << ":" << endl;
+    sex[order[0]] = 0;
+    if (!suspicious(interactions, order, 0, sex))
+      cout << "No ";
+    else
+      {
+	if (!suspicious(interactions, order, 0, sex))
+	  cout << "No ";
+      }
+    cout << "Suspicious bugs found!" << endl;
   }
   
   return 0;
 }
 
 void printInteractions (const vector<vector<unsigned int> >& interactions, const vector<pair<int,int> >& sizes) {
-  for (auto itr = sizes.begin(), end = sizes.end(); itr != end; itr++) {
+  for (vector<pair<int,int> >::const_iterator itr = sizes.begin(), end = sizes.end(); itr != end; itr++) {
     int idx = itr->first;
     cout << idx + 1 << ": ";
-    for (auto itr = interactions[idx].begin(); itr != interactions[idx].end(); itr++)
-      cout << *itr << " ";
+    for (vector<unsigned int>::const_iterator itr = interactions[idx].begin(); itr != interactions[idx].end(); itr++)
+      cout << *itr+1 << " ";
     cout << endl;
-      }
+  }
+}
+
+bool suspicious (const vector<vector<unsigned int> >& interactions, const vector<int>& order, int orderId , vector<char> sex) {
+  int numBugs = interactions.size();
+  int index = order[orderId];
+
+  // check for inconsistencies
+  for (vector<unsigned int>::const_iterator itr = interactions[index].begin() , end = interactions[index].end(); itr != end; itr++)
+    if (sex[*itr] == sex[index])
+      return true;
+
+  orderId ++;
+  if (orderId == numBugs)
+    return false;
+  
+  index = order[orderId];
+  if (sex[index] != 2) 
+    return suspicious(interactions, order, orderId, sex);
+    
+ else {
+   vector<char> sex_ (sex);
+   sex_[index] = 0;
+   if (!suspicious(interactions, order, orderId, sex_)) 
+     return false;
+   sex_[index] = 1;
+   if (!suspicious(interactions, order, orderId, sex_))
+     return false;
+ }
+  
+  return true; // a bit curious 
 }
